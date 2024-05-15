f1778a3dac06f5cd46e76df0b71fd4d5dd3ab987
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-05-15 16:22:54 +0530

    day4

---
+++ day5.py
@@ -0,0 +1,103 @@
+# dt = {'a':10,'b':20,'c':30,'d':[1,2,3,4,5]}
+
+# print(dt['d'])
+# dt['a'] = 50
+# print(dt.keys())
+# print(dt.values())
+# ls = [1,1,2,2,3,3,4,4,5,5,6,6,7,7]
+# print(set(ls))
+
+
+# type casting
+# set(),int(),float(),bool(),list(),tuple()
+
+
+
+# st = {1,1,2,2,3,3,4,4,5,5,6,6,7,7}
+
+# st.add(100)
+# print(st)
+
+# st.remove(101)
+# print("after removed : ",st)
+# st.discard(101)
+# print("after removed : ",st)
+
+ 
+
+ ## ARITHMATIC OPERATOR
+
+# num1 = int(input("pls enter first number : "))
+# num2 = int(input("pls enter second number : "))
+# print(type(num1))
+# result = num1 // num2
+
+# print(result)
+
+# conditional statement
+
+# if , else , elif,
+# num1 = int(input("pls enter first number : "))
+# num2 = int(input("pls enter second number : "))
+# if num1 > num2:
+#     print('Num 1 is greater')
+
+# elif num1 = num2:
+#     print('both are equal')
+
+
+# else:
+#     print('Num 2 is greater')
+
+# if num1 <= num2  :
+#     print('num 1 greater or equl')
+
+# else:
+#     print('not bigger and not equal')
+
+
+# age = int(input('plz enter your age : '))
+
+# if not age>24:
+#     print("you can vote")
+# else:
+#     print('you can not vote')
+
+
+
+# AND 
+#     1   1   -->   1
+#     0   1   -->   0
+#     1   0   -->   0
+
+
+# OR 
+
+# 0   1  --> 1
+# 1   0  --> 1
+# 0   0  --> 0
+
+# NOT 
+# 0  -->  1
+# 1  -->  0
+
+
+
+# =	              x = 5	              x = 5	
+# +=                x += 3	          x = x + 3	
+# -=	              x -= 3              x = x - 3	
+
+# num = 10 
+# num+= 1     #   num = num +  1
+# num*= 2     #   num = num -  1
+# print(num)
+
+
+# fruits = ['mango','banana','apple','grapes']
+# dt = {'apple':10,'banana':11,'grapes':25}
+
+# if "apple" in dt:
+#     print('it is available')
+# else:
+#     print('not avaialble')
+
